6c7a05ade2631de2c6acb955815b06a440924bfe
Evil_Avatar <tleung999@gmail.com>
2014-09-23 13:15:59 -0700

    fixed date parse

--- app/helpers/weapons_helper.rb
+++ app/helpers/weapons_helper.rb
@@ -4,7 +4,7 @@ module WeaponsHelper
     date_list = []
     sale_price_list = []
     item_object.each do |item|
-      date_list << item.created_at.to_s
+      date_list << Date.parse(item.created_at.to_s)
       offer_price_list << item.max_offer_unit_price/10000
       sale_price_list << item.min_sale_unit_price/10000
     end
